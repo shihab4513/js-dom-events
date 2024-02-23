5b4ced5fd9a77c28b3a6939cce47233aa8650541
shihab4513 <shihab4513@gmail.com>
2024-02-23 17:15:18 +0600

    Add Onclick Handler Directly Or Via Javascript

--- events.js
+++ events.js
@@ -1,1 +1,13 @@
 console.log('This is separate js files');
+// option 2
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+// We got access of the blue button by its id and printed in the console
+const makeBlueButton = document.getElementById('make-blue');
+// console.log(makeBlueButton);
+// Another way to do onclick function 
+makeBlueButton.onclick = makeBlue;
